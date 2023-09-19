ddaf44cf43ed7137ae01d40ba2522ca03a8b8917
Daniel Martinez <daniel.martinez.liu@enti.cat>
2023-09-19 16:31:36 +0200

    mainPush

---
+++ CDI_SistemaOperatius_23_24/CDI_SistemaOperatius_23_24/CDI_SistemaOperatius_23_24.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <chrono>
+
+#pragma region Fuctions Declaration Header
+
+void PrintElapsedTime(std::chrono::system_clock::time_point start, std::chrono::system_clock::time_point end);
+void Count(int maxCounter);
+void Example01();
+void Example02();
+void Example03();
+void Example04();
+void Example05();
+void Example06();
+
+
+#pragma endregion 
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
